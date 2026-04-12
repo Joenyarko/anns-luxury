9c16ce33346722c92b178c5ec8c345e618c07dcf
JoeNyarko <joenyarko2001@gmail.com>
2026-04-12 20:04:52 +0000

    Add Typescript module declarations for MP4 files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,3 +9,13 @@ declare module "*.MOV" {
   const src: string;
   export default src;
 }
+
+declare module "*.MP4" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
